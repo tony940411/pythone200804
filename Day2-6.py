220c055fff8ed860ef129d9f00b36bb72e608963
tony940411 <69131764+tony940411@users.noreply.github.com>
2020-08-04 21:54:46 +0800

    Update Day2-6.py

--- Day2-6.py
+++ Day2-6.py
@@ -2,6 +2,8 @@ x=[]
 scr=[]
 total=0
 a=0
+high=0
+low=999
 p=input("請輸入人數")
 p=int(p)
 for i in range(p):
@@ -10,5 +12,13 @@ for i in range(p):
     s=input("請輸入分數")
     scr.append(s)
 for i in range(p):
-    total=total+scr(p)
-print(total)
+    total=total+int(scr(p))
+    if int(scr(p)) > high:
+        high=int(scr(p))
+    if int(scr(p)) <low
+        iow=int(scr(p))
+a=total/p
+print("總和是"total)
+print("平均是"a)
+print("最高分數是"high)
+print("最低分數是"low)
